e053ea7878434f420201237488034ca563f5e2cd
leoleopon <safari2002@list.ru>
2021-01-14 15:48:48 +0500

    Implement test database creation scripts.

--- Tests/NewPlatform.Flexberry.AuditBigData.Tests/SqlScripts/MSSql.create.sql
+++ Tests/NewPlatform.Flexberry.AuditBigData.Tests/SqlScripts/MSSql.create.sql
@@ -84,6 +84,33 @@ CREATE TABLE [Class3] (
 	 PRIMARY KEY ([primaryKey]))
 
 
+CREATE TABLE [Audit] (
+
+	 [primaryKey] UNIQUEIDENTIFIER  NOT NULL,
+
+	 [UserName] VARCHAR(255)  NULL,
+
+	 [UserLogin] VARCHAR(255)  NULL,
+
+	 [ObjectType] VARCHAR(255)  NULL,
+
+	 [ObjectPrimaryKey] LONGVARBINARY  NULL,
+
+	 [OperationTime] DATETIME  NOT NULL,
+
+	 [OperationType] VARCHAR(255)  NOT NULL,
+
+	 [ExecutionStatus] VARCHAR(10)  NOT NULL,
+
+	 [Source] VARCHAR(255)  NULL,
+
+	 [SerializedFields] NVARCHAR(MAX)  NULL,
+
+	 [HeadAuditEntity] UNIQUEIDENTIFIER  NULL,
+
+	 PRIMARY KEY ([primaryKey]))
+
+
 CREATE TABLE [STORMNETLOCKDATA] (
 
 	 [LockKey] VARCHAR(300)  NOT NULL,
@@ -263,373 +290,14 @@ CREATE TABLE [ApplicationLog] (
 	 PRIMARY KEY ([primaryKey]))
 
 
-CREATE TABLE [STORMAG] (
 
-	 [primaryKey] uniqueidentifier  NOT NULL,
 
-	 [Name] varchar(80)  NOT NULL,
-
-	 [Login] varchar(50)  NULL,
-
-	 [Pwd] varchar(50)  NULL,
-
-	 [IsUser] bit  NOT NULL,
-
-	 [IsGroup] bit  NOT NULL,
-
-	 [IsRole] bit  NOT NULL,
-
-	 [ConnString] varchar(255)  NULL,
-
-	 [Enabled] bit  NULL,
-
-	 [Email] varchar(80)  NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 [Comment] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMLG] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Group_m0] uniqueidentifier  NOT NULL,
-
-	 [User_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMAuObjType] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Name] nvarchar(255)  NOT NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMAuEntity] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [ObjectPrimaryKey] nvarchar(38)  NOT NULL,
-
-	 [OperationTime] DATETIME  NOT NULL,
-
-	 [OperationType] nvarchar(100)  NOT NULL,
-
-	 [ExecutionResult] nvarchar(12)  NOT NULL,
-
-	 [Source] nvarchar(255)  NOT NULL,
-
-	 [SerializedField] nvarchar(max)  NULL,
-
-	 [User_m0] uniqueidentifier  NOT NULL,
-
-	 [ObjectType_m0] uniqueidentifier  NOT NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMAuField] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Field] nvarchar(100)  NOT NULL,
-
-	 [OldValue] nvarchar(max)  NULL,
-
-	 [NewValue] nvarchar(max)  NULL,
-
-	 [MainChange_m0] uniqueidentifier  NULL,
-
-	 [AuditEntity_m0] uniqueidentifier  NOT NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMI] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [User_m0] uniqueidentifier  NOT NULL,
-
-	 [Agent_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [Session] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [UserKey] uniqueidentifier  NULL,
-
-	 [StartedAt] datetime  NULL,
-
-	 [LastAccess] datetime  NULL,
-
-	 [Closed] bit  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMS] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Name] varchar(100)  NOT NULL,
-
-	 [Type] varchar(100)  NULL,
-
-	 [IsAttribute] bit  NOT NULL,
-
-	 [IsOperation] bit  NOT NULL,
-
-	 [IsView] bit  NOT NULL,
-
-	 [IsClass] bit  NOT NULL,
-
-	 [SharedOper] bit  NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 [Comment] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMP] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Subject_m0] uniqueidentifier  NOT NULL,
-
-	 [Agent_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMF] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [FilterText] varchar(MAX)  NULL,
-
-	 [Name] varchar(255)  NULL,
-
-	 [FilterTypeNView] varchar(255)  NULL,
-
-	 [Subject_m0] uniqueidentifier  NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMAC] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [TypeAccess] varchar(7)  NULL,
-
-	 [Filter_m0] uniqueidentifier  NULL,
-
-	 [Permition_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMLO] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Class_m0] uniqueidentifier  NOT NULL,
-
-	 [Operation_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMLA] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [View_m0] uniqueidentifier  NOT NULL,
-
-	 [Attribute_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMLV] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [Class_m0] uniqueidentifier  NOT NULL,
-
-	 [View_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-CREATE TABLE [STORMLR] (
-
-	 [primaryKey] uniqueidentifier  NOT NULL,
-
-	 [StartDate] datetime  NULL,
-
-	 [EndDate] datetime  NULL,
-
-	 [Agent_m0] uniqueidentifier  NOT NULL,
-
-	 [Role_m0] uniqueidentifier  NOT NULL,
-
-	 [CreateTime] datetime  NULL,
-
-	 [Creator] varchar(255)  NULL,
-
-	 [EditTime] datetime  NULL,
-
-	 [Editor] varchar(255)  NULL,
-
-	 PRIMARY KEY ([primaryKey]))
-
-
-
-
- ALTER TABLE [Class2] ADD CONSTRAINT [Class2_FClass1_0] FOREIGN KEY ([Class1]) REFERENCES [Class1]
-CREATE INDEX Class2_IClass1 on [Class2] ([Class1])
-
- ALTER TABLE [Class2] ADD CONSTRAINT [Class2_FClass4_0] FOREIGN KEY ([Class4]) REFERENCES [Class4]
-CREATE INDEX Class2_IClass4 on [Class2] ([Class4])
-
- ALTER TABLE [Class3] ADD CONSTRAINT [Class3_FClass2_0] FOREIGN KEY ([Class2]) REFERENCES [Class2]
-CREATE INDEX Class3_IClass2 on [Class3] ([Class2])
+ ALTER TABLE [Audit] ADD CONSTRAINT [Audit_FAudit_0] FOREIGN KEY ([HeadAuditEntity]) REFERENCES [Audit]
+CREATE INDEX Audit_IHeadAuditEntity on [Audit] ([HeadAuditEntity])
 
  ALTER TABLE [STORMWEBSEARCH] ADD CONSTRAINT [STORMWEBSEARCH_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
 
  ALTER TABLE [STORMFILTERDETAIL] ADD CONSTRAINT [STORMFILTERDETAIL_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
 
  ALTER TABLE [STORMFILTERLOOKUP] ADD CONSTRAINT [STORMFILTERLOOKUP_FSTORMFILTERSETTING_0] FOREIGN KEY ([FilterSetting_m0]) REFERENCES [STORMFILTERSETTING]
-
- ALTER TABLE [STORMLG] ADD CONSTRAINT [STORMLG_FSTORMAG_0] FOREIGN KEY ([Group_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMLG] ADD CONSTRAINT [STORMLG_FSTORMAG_1] FOREIGN KEY ([User_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMAuEntity] ADD CONSTRAINT [STORMAuEntity_FSTORMAG_0] FOREIGN KEY ([User_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMAuEntity] ADD CONSTRAINT [STORMAuEntity_FSTORMAuObjType_0] FOREIGN KEY ([ObjectType_m0]) REFERENCES [STORMAuObjType]
-
- ALTER TABLE [STORMAuField] ADD CONSTRAINT [STORMAuField_FSTORMAuField_0] FOREIGN KEY ([MainChange_m0]) REFERENCES [STORMAuField]
-
- ALTER TABLE [STORMAuField] ADD CONSTRAINT [STORMAuField_FSTORMAuEntity_0] FOREIGN KEY ([AuditEntity_m0]) REFERENCES [STORMAuEntity]
-
- ALTER TABLE [STORMI] ADD CONSTRAINT [STORMI_FSTORMAG_0] FOREIGN KEY ([User_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMI] ADD CONSTRAINT [STORMI_FSTORMAG_1] FOREIGN KEY ([Agent_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMP] ADD CONSTRAINT [STORMP_FSTORMS_0] FOREIGN KEY ([Subject_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMP] ADD CONSTRAINT [STORMP_FSTORMAG_0] FOREIGN KEY ([Agent_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMF] ADD CONSTRAINT [STORMF_FSTORMS_0] FOREIGN KEY ([Subject_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMAC] ADD CONSTRAINT [STORMAC_FSTORMF_0] FOREIGN KEY ([Filter_m0]) REFERENCES [STORMF]
-
- ALTER TABLE [STORMAC] ADD CONSTRAINT [STORMAC_FSTORMP_0] FOREIGN KEY ([Permition_m0]) REFERENCES [STORMP]
-
- ALTER TABLE [STORMLO] ADD CONSTRAINT [STORMLO_FSTORMS_0] FOREIGN KEY ([Class_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLO] ADD CONSTRAINT [STORMLO_FSTORMS_1] FOREIGN KEY ([Operation_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLA] ADD CONSTRAINT [STORMLA_FSTORMS_0] FOREIGN KEY ([View_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLA] ADD CONSTRAINT [STORMLA_FSTORMS_1] FOREIGN KEY ([Attribute_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLV] ADD CONSTRAINT [STORMLV_FSTORMS_0] FOREIGN KEY ([Class_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLV] ADD CONSTRAINT [STORMLV_FSTORMS_1] FOREIGN KEY ([View_m0]) REFERENCES [STORMS]
-
- ALTER TABLE [STORMLR] ADD CONSTRAINT [STORMLR_FSTORMAG_0] FOREIGN KEY ([Agent_m0]) REFERENCES [STORMAG]
-
- ALTER TABLE [STORMLR] ADD CONSTRAINT [STORMLR_FSTORMAG_1] FOREIGN KEY ([Role_m0]) REFERENCES [STORMAG]
 
