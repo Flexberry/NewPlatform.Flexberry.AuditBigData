82c5e782d25efefdab2771b4529c5f93f0cd4141
kafnevod <kaf@nevod.ru>
2021-03-12 11:31:02 +0000

    Update ClickHouseDirectGenerator.create.sql

--- SQL/ClickHouseDirectGenerator.create.sql
+++ SQL/ClickHouseDirectGenerator.create.sql
@@ -1,33 +1,22 @@
+CREATE TABLE AuditMergeTree
+(
+    "primaryKey" UUID,
+    "UserName" String,
+    "UserLogin" String,
+    "OperationId" Nullable(UUID),
+    "OperationTags" String,
+    "ObjectType" String,
+    "ObjectPrimaryKey" String,
+    "OperationTime" DateTime,
+    "OperationType" String,
+    "ExecutionStatus" String,
+    "Source" String,
+    "SerializedFields" String,
+    "HeadAuditEntity" Nullable(UUID)
+)
+ENGINE = MergeTree()
+ORDER BY primaryKey
+SETTINGS index_granularity = 8192;
 
 
-
-
-CREATE TABLE "Audit" (
-
- "primaryKey" UUID,
-
- "UserName" String,
-
- "UserLogin" String,
-
- "ObjectType" String,
-
- "ObjectPrimaryKey" String,
-
- "OperationTime" DateTime,
-
- "OperationType" String,
-
- "ExecutionStatus" String,
-
- "Source" String,
-
- "SerializedFields" StringMax,
-
- "HeadAuditEntity" UUID
-
-) ENGINE = MergeTree() ORDER BY ("primaryKey");
-
-
-
-
+CREATE TABLE Audit AS AuditMergeTree ENGINE = Buffer(test, AuditMergeTree, 16, 1, 2, 10, 10000, 100000, 1000000);
