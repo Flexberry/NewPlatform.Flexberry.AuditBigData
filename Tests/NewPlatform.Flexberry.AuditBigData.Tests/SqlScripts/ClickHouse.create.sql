85c0de2b63e92f1ebba426e8bfce843258e0a726
AntonIV8 <deimoss@mail.ru>
2021-06-23 17:36:39 +0500

    Update ado (#13)
    * Update ado
    
    * Install System.Runtime.CompilerServices.Unsafe
    
    * Update ado and sql scripts.
    
    Co-authored-by: ics_home\ananin <anton-iv87@yandex.ru>

--- Tests/NewPlatform.Flexberry.AuditBigData.Tests/SqlScripts/ClickHouse.create.sql
+++ Tests/NewPlatform.Flexberry.AuditBigData.Tests/SqlScripts/ClickHouse.create.sql
@@ -1,29 +1,22 @@
-CREATE TABLE "Audit" (
+CREATE TABLE Audit
+(
+    "primaryKey" UUID,
+    "UserName" Nullable(String),
+    "UserLogin" Nullable(String),
+    "OperationId" Nullable(UUID),
+    "OperationTags" String,
+    "ObjectType" Nullable(String),
+    "ObjectPrimaryKey" Nullable(String),
+    "OperationTime" DateTime,
+    "OperationType" String,
+    "ExecutionStatus" String,
+    "Source" Nullable(String),
+    "SerializedFields" Nullable(String),
+    "HeadAuditEntity" Nullable(UUID)
+)
+ENGINE = MergeTree()
+ORDER BY OperationTime
+SETTINGS index_granularity = 8192;
 
- "primaryKey" UUID,
 
- "UserName" String,
-
- "UserLogin" String,
-
- "OperationId" Nullable(UUID),
-
- "OperationTags" String,
-
- "ObjectType" String,
-
- "ObjectPrimaryKey" NVARCHAR(38),
-
- "OperationTime" DateTime,
-
- "OperationType" String,
-
- "ExecutionStatus" String,
-
- "Source" String,
-
- "SerializedFields" String,
-
- "HeadAuditEntity" Nullable(UUID)
-
-) ENGINE = MergeTree() ORDER BY ("OperationTime");
+CREATE TABLE AuditBuffer AS Audit ENGINE = Buffer(currentDatabase(), Audit, 16, 0.1, 2, 10, 10000, 100000, 1000000);
